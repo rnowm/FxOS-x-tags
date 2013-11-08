a8ca904c67aa2f64622bb73a35f65f32e7e028ba
Sam Joch <samuel@guiora.com>
2013-11-07 17:11:26 -0800

    work in progress

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test using mocha, sinon, Chai
--- src/headers.js
+++ src/headers.js
@@ -1,5 +1,8 @@
 (function() {
   // Helpers
+  var $ = function(selector) {
+    return document.getElementById(selector);
+  }
   Handlebars.registerHelper('em', function(counter) {
     var html = counter ? ' <em>' + counter + '</em>' : '';
     return new Handlebars.SafeString(html);
@@ -20,41 +23,85 @@
     }
   });
   var Helpers = {
-    created: function(name) {
-      var template = document.getElementById(name + '-template');
-      this.attrs = Components[name].attributes || [];
-      this.defaults = Components[name].defaultAttributes || {};
-      this.template = template.innerHTML;
-      this.render = Handlebars.compile(this.template);
+    replaceNode: function(newNode, oldNode) {
+      while(newNode.firstChild) {
+        oldNode.parentNode.insertBefore(newNode.firstChild, oldNode);
+      }
+      setTimeout(function() {
+        oldNode.parentNode && oldNode.parentNode.removeChild(oldNode);
+      }, 0);
     },
-    inserted: function() {
-      var data = { content: this.innerHTML };
-      this.attrs.forEach(function(attr) {
-        data[attr] = this.getAttribute(attr) || this.defaults[attr];
+    created: function(name) {
+      console.log('Created', name, this);
+      var data = {  };
+      (WebComponents[name].attributes || []).forEach(function(attr) {
+        data[attr] = this.getAttribute(attr)
+          || WebComponents[name].defaults[attr];
       }.bind(this));
       var holder = document.createElement('div');
-      holder.innerHTML = this.render(data);
-      this.parentElement.replaceChild(holder.firstElementChild, this);
+      holder.innerHTML = WebComponents[name].render(data);
+      var content = holder.querySelector('content');
+      if (content) {
+        for(var i = 0; i < this.childNodes.length; i++) {
+          if (this.childNodes[i].nodeName.match('BB-')) {
+            this.removeChild(this.childNodes[i]);
+          }
+        }
+        console.log('Content find! -> replace with', this.innerHTML);
+        content.innerHTML = this.innerHTML;
+        Helpers.replaceNode(this, content);
+      }
+      // this.parentElement.replaceChild(holder.firstElementChild, this);
+      // Trick !
+      console.log('Parent', name, this);
+      Helpers.replaceNode(holder, this);
     }
-  };
-  var Components = {
+  }
+  var WebComponents = window.WebComponents = {
     header: {
-      attributes: ['nav-id', 'nav-action', 'nav', 'heading', 'counter'],
-      defaultAttributes: {
+      render: Handlebars.compile($('header-template').innerHTML),
+      attributes: ['nav-id', 'nav-action', 'nav', 'heading', 'counter',
+        'theme'],
+      defaults: {
+        theme: null,
         heading: 'Default header'
       }
     },
     subheader: {
-      attributes: ['heading']
+      render: Handlebars.compile($('subheader-template').innerHTML),
+      attributes: ['heading'],
+      defaults: {}
+    },
+    list: {
+      render: Handlebars.compile($('list-template').innerHTML),
+      attributes: ['heading'],
+      defaults: {}
+    },
+    row: {
+      render: Handlebars.compile($('row-template').innerHTML),
+      attributes: ['icon'],
+      defaults: {}
     }
   };
-  Object.keys(Components).forEach(function(component) {
-    xtag.register('bb-' + component, {
-      lifecycle: {
-        created: function() { Helpers.created.call(this, component) },
-        inserted: Helpers.inserted
-      }
-    });
+  xtag.register('bb-header', {
+    lifecycle: {
+      created: function() { Helpers.created.call(this, 'header'); }
+    }
+  });
+  xtag.register('bb-subheader', {
+    lifecycle: {
+      created: function() { Helpers.created.call(this, 'subheader'); }
+    }
+  });
+  xtag.register('bb-list', {
+    lifecycle: {
+      created: function() { Helpers.created.call(this, 'list'); }
+    }
+  });
+  xtag.register('bb-row', {
+    lifecycle: {
+      created: function() { Helpers.created.call(this, 'row'); }
+    }
   });
 })();
 
